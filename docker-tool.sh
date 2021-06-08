bcff5c99a04c6cb264adc49fa102e2fb5227aa70
Muhammad Hussein Fattahizadeh <m@mhf.ir>
2021-06-08 08:38:45 +0430

    fix: test

--- docker-tool.sh
+++ docker-tool.sh
@@ -76,7 +76,7 @@ case "$subcommand" in
       build_args="$build_args --build-arg TESSERACT_LANGUAGES='${tesseract_languages}'"
     fi
     # Build slim version with minimal dependencies
-    # eval "docker build -t apache/tika:${version} ${build_args} - < minimal/Dockerfile --no-cache"
+    eval "docker build -t apache/tika:${version} ${build_args} - < minimal/Dockerfile --no-cache"
     # Build full version with OCR, Fonts and GDAL
     eval "docker build -t apache/tika:${version}-full ${build_args} - < full/Dockerfile"
     ;;
